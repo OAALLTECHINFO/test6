cd7f144821cf4761f92f4f08de29c26b61b58cd8
OAALLTECHINFO <102165261+OAALLTECHINFO@users.noreply.github.com>
2022-06-21 16:53:51 +0400

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<style>
+	@import url("https://unpkg.com/fluent-svelte/theme.css");
+	:global(body) {
+		background-color: var(--fds-solid-background-base);
+		color: var(--fds-text-primary);
+	}
+</style>
+
+<script>
+	import { TextBox } from "fluent-svelte";
+	import GradientText from 'svelte-gradient-typography';
+	let value = "";
+</script>
+
+<TextBox placeholder="Chose a Name" bind:value />
+
+<GradientText>
+    The {value}
+</GradientText>
